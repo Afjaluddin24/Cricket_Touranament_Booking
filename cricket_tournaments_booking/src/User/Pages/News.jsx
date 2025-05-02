12e31b9e363ee3c9306a006a27e83d635e1fc064
Afjaluddin24 <shekhafjal024@gmail.com>
2025-05-02 13:02:34 +0530

    update no

--- cricket_tournaments_booking/src/User/Pages/News.jsx
+++ cricket_tournaments_booking/src/User/Pages/News.jsx
@@ -1,11 +1,27 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Header from "../Header";
 import Footer from "../Footer";
 import Loadung from "../../Loadung";
+import { getData } from "../../APIConfig/ConfigAPI";
 
 function News(props) {
     const [isLoading,setIsLoading] = useState(true);
 
+    const [ListNews,setListNews] = useState([])
+
+    const getListNews = async () =>{
+      try {
+        const response = await getData("News/NewDisplayDetals")
+        setListNews(response.result);
+        console.log("Afjal",response.result);
+      } catch (error) {
+        console.log("Error",response.result);
+      }
+    }
+
+    useEffect(()=>{
+      getListNews()
+    },[])
     const timer = setTimeout(() => {
         setIsLoading(false);
     },2000);
@@ -35,146 +51,43 @@ function News(props) {
       <section id="contant" className="contant">
         <div className="container">
           <div className="row">
-            <div className="col-lg-9 col-sm-12 col-xs-12">
+            <div className="col-lg-12 col-sm-12 col-xs-12">
               <div className="news-post-holder">
-                <div className="col-lg-6 col-sm-6 col-xs-12">
-                  <div className="news-post-widget">
-                    <img
-                      className="img-responsive"
-                      src="./TempletFiles/images/img-01_002.jpg"
-                      alt=""
-                    />
-                    <div className="news-post-detail">
-                      <span className="date">20 march 2016</span>
-                      <h2>
-                        <a href="blog-detail.html">
-                          Free play to ground in anywhere
-                        </a>
-                      </h2>
-                      <p>
-                        Just hours after that his grandma had died, Angel Di
-                        Maria imagined how she might react if he didn't play
-                      </p>
-                    </div>
-                  </div>
-                </div>
-                <div className="col-lg-6 col-sm-6 col-xs-12">
-                  <div className="news-post-widget">
-                    <img
-                      className="img-responsive"
-                      src="./TempletFiles/images/img-01_002.jpg"
-                      alt=""
-                    />
-                    <div className="news-post-detail">
-                      <span className="date">20 march 2016</span>
-                      <h2>
-                        <a href="blog-detail.html">
-                          Free play to ground in anywhere
-                        </a>
-                      </h2>
-                      <p>
-                        Just hours after that his grandma had died, Angel Di
-                        Maria imagined how she might react if he didn't play
-                      </p>
-                    </div>
-                  </div>
-                </div>
-                <div className="col-lg-6 col-sm-6 col-xs-12">
-                  <div className="news-post-widget">
-                    <img
-                      className="img-responsive"
-                      src="./TempletFiles/images/img-01_002.jpg"
-                      alt=""
-                    />
-                    <div className="news-post-detail">
-                      <span className="date">20 march 2016</span>
-                      <h2>
-                        <a href="blog-detail.html">
-                          Free play to ground in anywhere
-                        </a>
-                      </h2>
-                      <p>
-                        Just hours after that his grandma had died, Angel Di
-                        Maria imagined how she might react if he didn't play
-                      </p>
-                    </div>
-                  </div>
-                </div>
-                <div className="col-lg-6 col-sm-6 col-xs-12">
-                  <div className="news-post-widget">
-                    <img
-                      className="img-responsive"
-                      src="./TempletFiles/images/img-01_002.jpg"
-                      alt=""
-                    />
-                    <div className="news-post-detail">
-                      <span className="date">20 march 2016</span>
-                      <h2>
-                        <a href="blog-detail.html">
-                          Free play to ground in anywhere
-                        </a>
-                      </h2>
-                      <p>
-                        Just hours after that his grandma had died, Angel Di
-                        Maria imagined how she might react if he didn't play
-                      </p>
-                    </div>
-                  </div>
-                </div>
+              {ListNews.map((o, index) => {
+  return (
+    <div className="col-lg-4 col-sm-4 col-xs-12" key={index}>
+      <div className="news-post-widget">
+        <img
+          className="img-responsive"
+          src={`http://192.168.210.121:12425/IMG/${o.imgs}`}
+          style={{height:"280px",width:"350px"}}
+          alt={o.title || "News Image"}
+        />
+        <div className="news-post-detail">
+          <span className="date" style={{ color: '#888', fontSize: '14px' }}>
+            {new Date(o.date).toLocaleDateString()}
+          </span>
+          <h2 style={{ fontSize: '20px', fontWeight: 'bold', color: '#333' }}>
+            <a href="#" style={{ textDecoration: 'none', color: '#0056b3' }}>
+              {o.title}
+            </a>
+          </h2>
+          <p style={{ fontSize: '15px', margin: '10px 0', color: '#555' }}>
+            {o.description}
+          </p>
+          <strong>Club:</strong> {o.cubName}<br/>
+          <strong>Player:</strong>{o.name}<br/>
+          <strong>Tournament:</strong> {o.tournamentName} ({o.tournamentType})<br/>
+          <strong>Total Score:</strong> {o.sore}
+        </div>
+      </div>
+    </div>
+  );
+})}
+
               </div>
             </div>
-            <div className="col-lg-3 col-sm-6 col-xs-12">
-              <div
-                className="content-widget top-story"
-                style={{
-                  background: "url(./TempletFiles/images/top-story-bg.jpg)",
-                }}
-              >
-                <div className="top-stroy-header">
-                  <h2>
-                    Top Soccer Headlines Story{" "}
-                    <a href="#" className="fa fa-fa fa-angle-right" />
-                  </h2>
-                  <span className="date">July 05, 2017</span>
-                  <h2>Other Headlines</h2>
-                </div>
-                <ul className="other-stroies">
-                  <li>
-                    <a href="#">Wenger Vardy won't start</a>
-                  </li>
-                  <li>
-                    <a href="#">Evans: Vardy just</a>
-                  </li>
-                  <li>
-                    <a href="#">Pires and Murray </a>
-                  </li>
-                  <li>
-                    <a href="#">Okazaki backing</a>
-                  </li>
-                  <li>
-                    <a href="#">Wolfsburg's Rodriguez</a>
-                  </li>
-                  <li>
-                    <a href="#">Jamie Vardy compared</a>
-                  </li>
-                  <li>
-                    <a href="#">Arsenal target Mkhitaryan</a>
-                  </li>
-                  <li>
-                    <a href="#">Messi wins libel case.</a>
-                  </li>
-                </ul>
-              </div>
-              <aside id="sidebar" className="right-bar">
-                <div className="banner">
-                  <img
-                    className="img-responsive"
-                    src="./TempletFiles/images/adds-3.jpg"
-                    alt="#"
-                  />
-                </div>
-              </aside>
-            </div>
+           
           </div>
         </div>
       </section>
